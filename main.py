05732464dd6e273c68d7f4b64a123dbe6d898bcd
rashwana2 <rashwana2@coventry.ac.uk>
2023-06-10 09:40:52 +0100

    added robots.txt retrieval

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import socket
 import sys
 import subprocess
 import multiprocessing
+import requests
 print(''' _              _              
 | |            | |             
 | | ___ __ __ _| | _____ _ __  
@@ -11,7 +12,7 @@ print(''' _              _
 |   <| | | (_| |   <  __/ | | |
 |_|\_\_|  \__,_|_|\_\___|_| |_|
 --------------------v1.0-------''')
-
+print('   --> Ahmed Rashwan || @Rsasec0x01')
 
 # scanning the host for ports
 def scan_host(host, port):
@@ -35,18 +36,28 @@ def run_nmap_service_scan(target):
         print(output)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running Nmap: {e.output}")
+# attempt retrieving content of robots.txt file, if found
+def check_robots_txt(host):
+    # Construct the URL for the robots.txt file
+    robots_url = f"{host}/robots.txt"
 
-# Usage example
-target_ip = "192.168.0.1"  # Replace with your target IP address
-run_nmap_service_scan(target_ip)
+    # Send a GET request to the URL
+    response = requests.get(robots_url)
+
+    if response.status_code == 200:
+        print('<<<------------Retrieved Content of Robots.txt File---------------->>>')
+        print(response.text)
+    else:
+        print("No robots.txt file found.")
+
 
 # script arguments
 parser = argparse.ArgumentParser(description='Port Scanner')
 parser.add_argument('--host', help='Specify the host for port scanning')
 
-
 args = parser.parse_args()
 if args.host:
+    print('<<<------------Port Scan Info---------------->>>')
     print(f"Started a Common Port Scan on host: {args.host}")
     open_ports = []
     for port_no in range(1, 1001):
@@ -55,6 +66,11 @@ if args.host:
     for open_port in open_ports:
         if open_port == 80:
             print(f"Web Server Detected on Port 80 for host {args.host}")
+            print('<<<-----------------Gobuster Results---------------->>>')
             run_gobuster(args.host)
+            print('Attempting to Retrieve Robots.txt file')
+            check_robots_txt(args.host)
+    print('<<<-----------------Nmap Service Scan Results---------------->>>')
     print('running nmap service scan on host...')
     run_nmap_service_scan(args.host)
+
